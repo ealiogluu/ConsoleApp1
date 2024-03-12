c74a478ef8898e8e9cf6395cb2d5ea46781b3a6f
emine nur <Lenovo@DESKTOP-50D6PVA>
2024-03-12 17:22:03 +0300

    ı

---
+++ BookStoreForms/Book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStoreForms
+{
+    internal class Book
+    {
+        public string Name { get; set; }
+        public string Writer { get; set; }
+        public string Publisher { get; set; }
+        public int PageCount { get; set; }
+
+        public Book(string name, string writer, string publisher, int pageCount)
+        {
+            Name = name;
+            Writer = writer;
+            Publisher = publisher;
+            PageCount = pageCount;
+        }
+    }
+}
---
+++ BookStoreForms/BookStore.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStoreForms
+{
+    internal class BookStore
+    {
+        List<Book> books = new List<Book>();
+        public BookStore()
+        {
+            Book book1 = new Book("İnsan Ne ile Yaşar?", "Tolstoy", "Can Yayınları", 67);
+        }
+
+    }
+}
